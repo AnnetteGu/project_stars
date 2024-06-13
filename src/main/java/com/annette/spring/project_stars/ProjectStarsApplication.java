7ffe65fe82b1d8ca3a39cda0eafd8dd25a0879c0
Annette <annetteremain@gmail.com>
2024-06-13 18:21:03 +0300

    базовая настройка проекта project_stars

---
+++ src/main/java/com/annette/spring/project_stars/ProjectStarsApplication.java
@@ -0,0 +1,13 @@
+package com.annette.spring.project_stars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectStarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectStarsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/annette/spring/project_stars/ProjectStarsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.annette.spring.project_stars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectStarsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
